9c0574aa571c2594808a3aa333b6db0bd7679647
Scovia Achan <achanscovia@gmail.com>
2023-03-19 17:26:02 +0000

    c file that prints a line of text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/**
+ * Creates a text line to the output
+ */
+
 int main(void)
 {
-
-	puts("Programming is like building a multilingual puzzle\n");
+	puts('"Programming is like building a multilingual puzzle"\n');
 	return (0);
 }
