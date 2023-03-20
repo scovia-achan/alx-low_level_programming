31ac60c01e20599997049a91a1030bba1b6b7749
Scovia Achan <achanscovia@gmail.com>
2023-03-20 21:50:53 +0000

    print alphabet in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	{
+		putchar(alph);
+	}
+
+	putchar('\n');
+	return (0);
+}
