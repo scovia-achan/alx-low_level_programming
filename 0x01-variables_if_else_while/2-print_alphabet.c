25bf73709a225f12802837b624e1f166e98548ea
Scovia Achan <achanscovia@gmail.com>
2023-03-20 21:22:01 +0000

    write alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alp++)
+	{
+		putchar(alph)
+	}
+	return (0);
+}
