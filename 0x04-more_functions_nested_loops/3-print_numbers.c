3d78f5c2869acdf3330df7f7d4bf7fadb8e41c2b
Scovia Achan <achanscovia@gmail.com>
2023-03-23 21:22:55 +0000

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
