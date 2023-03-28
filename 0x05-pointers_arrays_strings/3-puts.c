4a04fb9298c5c6b20e70160860e8f66bd403d535
Scovia Achan <achanscovia@gmail.com>
2023-03-28 19:38:05 +0000

    function that prints string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed buy a new line
+ * @str: parameter
+ * return: always zero
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar('\n');
+}
