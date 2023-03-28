b0251798e5baf6783347dd5ee4544c712c8cc9ed
Scovia Achan <achanscovia@gmail.com>
2023-03-28 20:11:42 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a string in reverse
+ * @s: parameter
+ * return: returns 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int str_len;
+	int temp;
+
+	str_len = strlen(s);
+	middle = str_len / 2;
+
+	for (i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[str_len - i - 1];
+		s[str_len - i - 1];
+	}
+}
