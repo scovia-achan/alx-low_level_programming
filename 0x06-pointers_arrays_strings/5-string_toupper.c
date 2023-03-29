130e0dbf69daa1d70b458650c8effcb962ea6be4
Scovia Achan <achanscovia@gmail.com>
2023-03-29 21:45:24 +0000

    change logic

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,12 @@ char *string_toupper(char *s)
 	int i;
 
 	/*Check if character is uppercase by checking it's ASCII value*/
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] -= 32;
 		}
 	}
+	return (s);
 }
