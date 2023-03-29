84054d2602aad7d0e5e467ef3ecc723d841e84bc
Scovia Achan <achanscovia@gmail.com>
2023-03-29 21:40:26 +0000

    add parameter and correct betty errors

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - print string to uppercase
+ * @s: string parameter
+ * Return: string in uppercase
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	/*Check if character is uppercase by checking it's ASCII value*/
+	for (i = 0; *[i]; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+}
