9a9c9725132cda18612a3bfb588370d6fe0f0193
Scovia Achan <achanscovia@gmail.com>
2023-04-03 22:31:22 +0000

    locate string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,10 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			return (s + i);
+			return (s[i]);
 		}
 	}
 	if (s[i] == c)
-		return (s + i);
+		return (s[i]);
 	return (NULL);
 }
