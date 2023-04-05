8b9c2dc96a2e27a1b1fa4b9075c72b1c0d862770
Scovia Achan <achanscovia@gmail.com>
2023-04-05 19:11:06 +0000

    recursively calculate the value of x to the power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of a number raised to a specific power
+ * @x: the first parameter
+ * @y: the power to raise x to
+ * Return: value of x after raising it to the power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	double res = 1;
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		res = res * _pow_recursion(x, y - 1);
+		return (res);
+	}
