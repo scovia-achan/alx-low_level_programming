9f6be4da195a34a22b56de6d5f3c5bea7213e2ff
Scovia Achan <achanscovia@gmail.com>
2023-04-05 09:14:35 +0000

    implement puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _puts_recursion - prints a character followed by a new line
  * @s: character to print
@@ -8,6 +8,9 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	_putchar('\n');
+	for (int i = 0; i <= strlen(s); i++)
+	{
+		_putchar(*s);
+		_putchar('\n');
+	}
 }
