f77f1bc0d76279df8b4dbf2bd93dc1320dcaf2cf
Scovia Achan <achanscovia@gmail.com>
2023-04-15 21:44:02 +0000

    Add readme print number of arguments to a program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the number of arguments passsed to it
+ * @argc:first argument
+ * @argv:second argument
+ * Return: number of arguments
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	printf("%d\n", argc);
+	return (0);
+}
