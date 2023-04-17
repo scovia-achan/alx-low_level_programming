1359cafe095822b0de81505555ed73e2deafc796
Scovia Achan <achanscovia@gmail.com>
2023-04-17 17:48:26 +0000

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * check - checks if an input is a digit
+ * @str: string to check
+ * Returns: True or false
+ */
+
+int check(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (isdigit(str[count]))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * main - adds positive numbers supplied as arguments
+ * @argc: parameter count (parameter 1)
+ * @argv: parameter 2
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int sum, i;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (check(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+}
