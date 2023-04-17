cac01ef0f71d1502e9c0b0ce11ef39c4b3287778
Scovia Achan <achanscovia@gmail.com>
2023-04-17 07:13:05 +0000

    multiplies to numbers supplied as arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies 2 numbers
+ * @argc: parameter count
+ * @argv: parameter 2
+ * Return: product of 2 numbers
+ */
+
+int main(int argc, char **argv)
+{
+	int res, i;
+
+	res = 1;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
