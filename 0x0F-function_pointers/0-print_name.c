8919597898fd2b3cc1cda89e8328bf107b612a3c
Scovia Achan <achanscovia@gmail.com>
2023-04-19 21:54:24 +0000

    print name of a person

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: second parameter
+ * Return: name of person
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}
