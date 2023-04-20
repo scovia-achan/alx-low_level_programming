57a82f6945663e8f34cfbf5f06b44df4ff8314ff
Scovia Achan <achanscovia@gmail.com>
2023-04-20 19:29:50 +0000

    print index of first element that's an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare variables
+ * Return: index of the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(arry[i]))
+			return (i);
+	}
+	return (-1);
+}
