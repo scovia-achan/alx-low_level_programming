f84c24a16f3c9544fa0f8f5581c28a2686931d8a
Scovia Achan <achanscovia@gmail.com>
2023-04-21 20:36:00 +0000

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,8 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
+
+		printf("Age: %.6f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
