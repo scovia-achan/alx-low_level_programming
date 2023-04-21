2adc741db9bfa1d078194f09c0c7b23ea46626e0
Scovia Achan <achanscovia@gmail.com>
2023-04-21 19:22:22 +0000

    initialize dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog function
+ * @d: dog struct initiation
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner
+ * Return: dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
