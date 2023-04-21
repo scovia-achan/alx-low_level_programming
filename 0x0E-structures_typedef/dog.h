abf9fead7c8e0bf811901f7b6476ccdb74cfe628
Scovia Achan <achanscovia@gmail.com>
2023-04-21 18:37:06 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct representing dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog owner's name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
