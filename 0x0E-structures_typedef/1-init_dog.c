9b2a2aea4295af0206cd3fdf24c3b799f60316c4
Scovia Achan <achanscovia@gmail.com>
2023-04-21 19:29:25 +0000

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
-	if (d == NULL)
-		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	/* check if the details arenot empty */
+
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
