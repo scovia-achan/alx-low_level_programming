d33849e7afda5f504a22c4eba929afe6d9f69715
Scovia Achan <achanscovia@gmail.com>
2023-04-23 18:40:32 +0000

    Add header file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,11 @@
+#ifndef 3_CALC_H
+#define 3_CALC_H
+#include <stdio.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif /* 3_CALC_H */
