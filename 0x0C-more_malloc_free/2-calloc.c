da1ccf7038afc8be244ddf57187983068e4ae230
Scovia Achan <achanscovia@gmail.com>
2023-04-26 20:51:29 +0000

    correct typo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	t = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
